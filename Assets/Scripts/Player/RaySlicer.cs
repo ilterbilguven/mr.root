3c232c437b05a1285c2b46dc0b68dcad3689a013
İlter Bilgüven <ilter@live.com>
2023-02-04 18:16:54 +0300

    root slicing added

--- Assets/Scripts/Player/RaySlicer.cs
+++ Assets/Scripts/Player/RaySlicer.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using MrRoot.Root;
+using UnityEngine.Serialization;
 
 namespace MrRoot.Player
 {
@@ -12,7 +14,8 @@ namespace MrRoot.Player
         private MrRootInput _input;
 
         [ReadOnly] [SerializeField] private bool _pressed;
-        [ReadOnly] [SerializeField] private Vector2 _position;
+        [ReadOnly] [SerializeField] private Vector2 _screenPosition;
+        [ReadOnly] [SerializeField] private Vector3 _previousWorldPosition;
         
 
         private Camera _mainCamera;
@@ -45,7 +48,7 @@ namespace MrRoot.Player
                 return;
             }
 
-            _position = obj.ReadValue<Vector2>();
+            _screenPosition = obj.ReadValue<Vector2>();
         }
 
         private void OnPressCanceled(InputAction.CallbackContext obj)
@@ -60,17 +63,49 @@ namespace MrRoot.Player
 
         private void FixedUpdate()
         {
-            var ray = MainCamera.ScreenPointToRay(_position);
+            var ray = MainCamera.ScreenPointToRay(_screenPosition);
+            
 
-            if (Physics.Raycast(ray, out var planeHit, LayerMask.GetMask("Water")))
+            if (Physics.Raycast(ray, out var planeHit, 1000f, LayerMask.GetMask("Water")))
             {
+                _previousWorldPosition = transform.position;
                 transform.position = planeHit.point + Vector3.up;
+                var velocity = transform.position - _previousWorldPosition;
+                
+                if (Physics.Raycast(ray, out var rootHit, 1000f, LayerMask.GetMask("Root")))
+                {
+                    if (rootHit.transform.TryGetComponent(out RootSlice rootSlice))
+                    {
+                        var normal = Vector3.Cross(-velocity, Vector3.up);
+                        
+                        DrawPlane(rootHit.point, normal);
+                    
+                        rootSlice.Slice(normal, rootHit.point);
+                    }
+                }
             }
-
-            if (Physics.Raycast(ray, out var rootHit, LayerMask.GetMask("Root")))
-            {
-                // todo: get root and cut
-            }
+        }
+        
+        public void DrawPlane(Vector3 position, Vector3 normal)
+        {
+            Vector3 v3;
+            if (normal.normalized != Vector3.forward)
+                v3 = Vector3.Cross(normal, Vector3.forward).normalized * normal.magnitude;
+            else
+                v3 = Vector3.Cross(normal, Vector3.up).normalized * normal.magnitude; ;
+            var corner0 = position + v3;
+            var corner2 = position - v3;
+            var q = Quaternion.AngleAxis(90.0f, normal);
+            v3 = q * v3;
+            var corner1 = position + v3;
+            var corner3 = position - v3;
+            Debug.DrawLine(corner0, corner2, Color.green, 1f);
+            Debug.DrawLine(corner1, corner3, Color.green, 1f);
+            Debug.DrawLine(corner0, corner1, Color.green, 1f);
+            Debug.DrawLine(corner1, corner2, Color.green, 1f);
+            Debug.DrawLine(corner2, corner3, Color.green, 1f);
+            Debug.DrawLine(corner3, corner0, Color.green, 1f);
+            Debug.DrawRay(position, normal, Color.red, 1f);
         }
     }
 }
